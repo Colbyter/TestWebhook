c1dccd96ef6251e144df1914c0aca5a47c61f224
Colbyter <colbyter@gmail.com>
2016-08-09 14:12:21 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,16 @@
+from itty import get,post, run_itty
+import json
+
+@post('/index.json')
+def index(request):
+    print json.loads(request.body.read())
+    #print sd
+
+    return 'Hello World!'
+
+    #for key in res:
+        
+      # mess = res["salut"]
+    
+
+run_itty(server='wsgiref', host='localhost', port=8003)
