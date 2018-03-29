39e7b75c672257e40d2291f9679ca8244e02ed69
Colbyter <colbyter@gmail.com>
2018-03-29 20:24:29 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,18 +4,10 @@ import json
 @post('/index.json')
 def index(request):
     print json.loads(request.body.read())
-    #print sd
 
     return 'Hello World!'
 
-    #for key in res:
-        
-      # mess = res["salut"]`dsdfsdf
-    
+
 
 run_itty(server='wsgiref', host='localhost', port=8003)
 
-cxxcv
-dfdfdsfsdf
-vvxcv
-dsfsdf
